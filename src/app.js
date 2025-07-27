03acc8404edd88701b013363fa2a028f7ca497ff
Srijan Shukla <srijanshukla353@gmail.com>
2025-07-27 13:56:22 +0530

    Move password validation and JWT generation into user model methods

--- src/app.js
+++ src/app.js
@@ -42,12 +42,14 @@ app.post("/login", async(req,res) => {
 
         }
 
-        const isPasswordValid = await bcrypt.compare(password,user.password) 
+        const isPasswordValid = await user.validatePassword(password)
         if(isPasswordValid ){
 
-            const token = jwt.sign({ _id: user._id }, "SecretKey")
+            const token = await user.getJWT()
 
-            res.cookie("token",token)
+            res.cookie("token", token, {
+            expires: new Date(Date.now() + 8 * 3600000),
+            });
             
             res.send("Login Successful ")
 
