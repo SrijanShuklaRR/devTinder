9aa740dfaf1907374e07a2ff5a779a95354c9122
Srijan Shukla <srijanshukla353@gmail.com>
2025-08-16 19:39:00 +0530

    geneate profile edit api

--- src/app.js
+++ src/app.js
@@ -1,148 +1,26 @@
-const express = require("express")
-const User = require("./models/user")
-const connectDB=require("./config/db")
-const app=express()
-const {validateSignUpData} = require("./utils/validation")
-const bcrypt = require("bcrypt")
-const cookieParser = require("cookie-parser")
-const jwt = require("jsonwebtoken")
-const { userAuth } = require("./middlewares/auth");
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+const cookieParser = require("cookie-parser");
 
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cookieParser())
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-app.post('/signup',async (req,res)=>{
-
-    const {firstName,lastName,emailId,password} = req.body
-    try{
-        validateSignUpData(req)
-        const passwordHash = await bcrypt.hash(password,10)
-        const user = new User({
-            firstName,lastName,emailId,password:passwordHash
-        })
-
-        
-        await user.save()
-        res.send("user added successfully")
-        }
-    catch(err){
-        res.status(400).send("Error: "+ err.message)
-    }    
-    
-})
-
-app.post("/login", async(req,res) => {
-    try {
-        
-        const {emailId,password} = req.body
-        const user = await User.findOne({emailId:emailId})
-        if(!user){
-            throw new Error("invalid credentials")
-
-        }
-
-        const isPasswordValid = await user.validatePassword(password)
-        if(isPasswordValid ){
-
-            const token = await user.getJWT()
-
-            res.cookie("token", token, {
-            expires: new Date(Date.now() + 8 * 3600000),
-            });
-            
-            res.send("Login Successful ")
-
-        }else{
-            throw new Error("invalid credentials")
-        }
-
-
-    } catch (error) {
-        res.status(400).send("ERROR :"+err.message)
-    }
-})
-
-app.get("/profile", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-
-    res.send(user);
-  } catch (err) {
-    res.status(400).send("ERROR : " + err.message);
-  }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  
-
-  res.send(user.firstName + "sent the connect request!");
-});
-
-
-app.get("/user",async (req,res)=>{
-
-    const userEmail=req.body.emailId
-    try{
-        const user = await User.find({emailId:userEmail})
-        if(user.length === 0){
-            res.status(400).send("user not found")
-        }
-        else{
-            res.send(user)
-        }
-        
-    } catch(err){
-        res.status(400).send("something went wrong")
-    }
-})
-
-app.get("/feed",async (req,res)=>{
-
-    try{
-        const users = await User.find({})
-        res.send(users)
-    }catch(err){
-        res.status(400).send("somethin went wrong")
-    }
-
-})
-
-app.delete("/user",async (req,res)=>{
-    const userId = req.body.userID
-    try{
-        await User.findByIdAndDelete(userId)
-        res.send("user deleted successfully")
-
-    }catch(err){
-        res.status(400).send("something went wrong")
-    }
-})
-
-app.patch("/user/:userId",async (req,res)=>{
-    const userId = req.params?.userId
-    const data=req.body
-    try{
-        const ALLOWED_UPDATES=["about","skills","gender","photoUrl"]
-        const isUpdateAllowed= Object.keys(data).every((k)=> ALLOWED_UPDATES.includes(k))
-        if(!isUpdateAllowed){
-            throw new Error("Update not allowed")
-        }
-        await User.findByIdAndUpdate({_id:userId},data,{runValidators:true})
-        res.send("user updated successfully")
-    }catch(err){
-        res.status(400).send("update failed"+err.message)
-    }
-})
-
-
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB()
-.then(()=>{
-    console.log("db connected")
-    app.listen(3000,()=>{console.log("server listening on port 3000")})
-})
-.catch(err=>{
-console.error("db not connected")
-})
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
